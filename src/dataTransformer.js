d462e55281c329d4bd9e591090966286473e2b47
Mark Dunbavan <mdunbavan@gmail.com>
2020-06-18 12:32:36 +0100

    commit shopify app

---
+++ src/dataTransformer.js
@@ -0,0 +1,57 @@
+import get from 'lodash/get';
+import last from 'lodash/last';
+import flatten from 'lodash/flatten';
+
+/**
+ * Transforms the API response of Shopify into
+ * the product schema expected by the SkuPicker component
+ */
+export const dataTransformer = product => {
+  const image = get(product, ['image', 'src'], '');
+  const sku = get(product, ['sku'], '');
+
+  return {
+    id: product.id,
+    image,
+    name: product.title,
+    sku
+  };
+};
+
+export const productsToVariantsTransformer = products =>
+  flatten(
+    products.map(product => {
+      const variants = product.variants.map(variant => ({
+        ...variant,
+        variantSKU: variant.sku,
+        sku: variant.id,
+        productId: product.id,
+        title: product.title,
+        hasNextPage: false
+      }));
+      variants[variants.length - 1].hasNextPage = product.hasNextPage;
+      return variants;
+    })
+  );
+
+export const previewsToVariants = ({ apiEndpoint }) => ({ sku, id, image, product }) => {
+  const productIdDecoded = atob(product.id);
+  const productId =
+    productIdDecoded && productIdDecoded.slice(productIdDecoded.lastIndexOf('/') + 1);
+  return {
+    id,
+    image: get(image, ['src'], ''),
+    // TODO: Remove sku:id when shared-sku-app supports internal IDs
+    // as an alternative piece of info to persist instead of the SKU.
+    // For now this is a temporary hack.
+    sku: id,
+    productId: product.id,
+    name: product.title,
+    ...(apiEndpoint &&
+      productId && {
+        externalLink: `https://${apiEndpoint}${
+          last(apiEndpoint) === '/' ? '' : '/'
+        }admin/products/${productId}`
+      })
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import { setup, renderSkuPicker } from 'shared-sku-app';
+import { fetchProductPreviews, makeProductSearchResolver } from './productResolvers';
+
+import logo from './logo.svg';
+
+const DIALOG_ID = 'dialog-root';
+
+function makeCTA(fieldType) {
+  return fieldType === 'Array' ? 'Select products' : 'Select a product';
+}
+
+export function validateParameters(parameters) {
+  if (parameters.storefrontAccessToken.length < 1) {
+    return 'Provide the storefront access token to your Shopify store.';
+  }
+
+  if (parameters.apiEndpoint.length < 1) {
+    return 'Provide the Shopify API endpoint.';
+  }
+
+  return null;
+}
+
+async function renderDialog(sdk) {
+  const container = document.createElement('div');
+  container.id = DIALOG_ID;
+  container.style.display = 'flex';
+  container.style.flexDirection = 'column';
+  document.body.appendChild(container);
+
+  renderSkuPicker(DIALOG_ID, {
+    sdk,
+    fetchProductPreviews,
+    fetchProducts: await makeProductSearchResolver(sdk)
+  });
+
+  sdk.window.startAutoResizer();
+}
+
+async function openDialog(sdk, currentValue, config) {
+  const skus = await sdk.dialogs.openCurrentApp({
+    allowHeightOverflow: true,
+    position: 'center',
+    title: makeCTA(sdk.field.type),
+    shouldCloseOnOverlayClick: true,
+    shouldCloseOnEscapePress: true,
+    parameters: config,
+    width: 1400
+  });
+
+  return Array.isArray(skus) ? skus : [];
+}
+
+function isDisabled(/* currentValue, config */) {
+  // No restrictions need to be imposed as to when the field is disabled from the app's side
+  return false;
+}
+
+setup({
+  makeCTA,
+  name: 'Shopify',
+  logo,
+  description:
+    'The Shopify app allows editors to select products from their Shopify account and reference them inside of Contentful entries.',
+  color: '#212F3F',
+  parameterDefinitions: [
+    {
+      id: 'storefrontAccessToken',
+      name: 'Storefront Access Token',
+      description: 'The storefront access token to your Shopify store',
+      type: 'Symbol',
+      required: true
+    },
+    {
+      id: 'apiEndpoint',
+      name: 'API Endpoint',
+      description: 'The Shopify API endpoint',
+      type: 'Symbol',
+      required: true
+    }
+  ],
+  fetchProductPreviews,
+  renderDialog,
+  openDialog,
+  isDisabled,
+  validateParameters
+});
