f67dd793d197a722253865a4a5a5d7dd64d5d5a4
Mark Dunbavan <mdunbavan@gmail.com>
2020-06-22 15:44:41 +0100

    use netlify lambda

--- express/server.js
+++ express/server.js
@@ -6,11 +6,23 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-app.use(express.static('./public'));
 
-app.get('/', (_, res) => {
-  res.sendFile(path.resolve(__dirname, '../index.html'));
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>App install running</h1>');
+  res.write('<script src="./bundle.js"></script>');
+  res.end();
 });
+
+// router.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../index.html'));
+//   res.end();
+// });
+
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router); // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
